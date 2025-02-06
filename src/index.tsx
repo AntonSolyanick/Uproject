77e5cf7b0e66bae993355b7ee4562c0fd86c8af0
REUS <antonsolyanick@gmail.com>
2025-02-06 22:40:19 +0300

    added gestures, react spring, dynamic lib loading with AnimationProvider

--- src/index.tsx
+++ src/index.tsx
@@ -10,7 +10,9 @@ import { ErrorBoundary } from './app/providers/ErrorBoundary';
 const container = document.getElementById('root');
 
 if (!container) {
-    throw new Error('Контейнер root не найден. НЕ удалось вмонтировать реакт приложение');
+    throw new Error(
+        'Контейнер root не найден. НЕ удалось вмонтировать реакт приложение'
+    );
 }
 
 const root = createRoot(container);
@@ -24,5 +26,5 @@ root.render(
                 </ThemeProvider>
             </ErrorBoundary>
         </StoreProvider>
-    </BrowserRouter>,
+    </BrowserRouter>
 );
